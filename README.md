6747a8b21bad8937d0ca183f53ed001e77c8eb06
Tyler Morgan <tylermorgan86@gmail.com>
2014-06-17 12:00:48 -0700

    adding disconnect code

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ client/server TCP communication
 ================================
 Client and server communication over a tcp connection
 Input is expected to be JSON 
+
--- server.js
+++ server.js
@@ -4,11 +4,18 @@ var sockets = [];
 
 var server = net.createServer(function(socket) {
   sockets.push(socket);
+  socket.num = sockets.length;
 
   socket.setEncoding('utf-8');
   socket.on('data', function(data) {
     writeToAll(data);
   });
+
+  socket.on('close', function(){
+    console.log(this.num + 'disconnected');
+    sockets.splice(this.num - 1 ,1);
+    console.dir(sockets);
+  })
 });
 
 server.listen(3000, '127.0.0.1');
